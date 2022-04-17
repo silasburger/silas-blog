cf85244dc15da73c98c6256ad8f7efd72a8ceb1c
Silas Burger <ybsilas@gmail.com>
2022-04-17 21:15:52 +0200

    worked on styling of header and changed some labels / font stuff

--- src/components/LanguageSwitcher/index.jsx
+++ src/components/LanguageSwitcher/index.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { Link, useI18next } from "gatsby-plugin-react-i18next";
 import { IoGlobeOutline } from "react-icons/io5";
+import './styles.scss';
 
 function LanguageSwitcher({ language }) {
   const { originalPath } = useI18next();
+  let link;
 
   if (language === "en") {
-    return (
+    link = (
       <Link to={originalPath} language={'es'}>
-        <IoGlobeOutline /> EN
+        <IoGlobeOutline /> en
       </Link>
     )
   } else {
-    return (
+    link = (
       <Link
         to={originalPath}
         language={'en'}
       >
-        <IoGlobeOutline /> ES
+            <IoGlobeOutline /> es
       </Link>
     )
   }
+  return (
+    <div className="link-wrapper language-switcher">
+      {link}
+    </div>
+  );
 }
 export default LanguageSwitcher
